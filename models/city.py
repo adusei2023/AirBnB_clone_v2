6957e38f88bfdc798a82c2ef0ce2f5df51696303
adusei2023 <samuel.boateng01@yahoo.com>
2024-06-23 15:33:52 -0700

    DBStorage - User

--- models/city.py
+++ models/city.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+"""This is HBNB the city class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
+from models.place import Place
+from os import getenv
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """This is the class for City
+    """
     __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    places = relationship('Place', backref="cities",
-                          cascade="all, delete, delete-orphan")
+
+    if getenv('HBNB_TYPE_STORAGE') is not None:
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", cascade='all, delete',
+                              backref="cities", passive_deletes=True)
+    else:
+        name = ""
+        state_id = ""
