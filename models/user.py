980766b1451fc9c0cab511a4ff34c16844e82a63
global506 <gloriaboateng2011@gmail.com>
2024-06-23 21:29:09 +0000

    user

--- models/user.py
+++ models/user.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""This is the user class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
+from os import getenv
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """This is the class for user"""
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship('Place', backref="user",
-                          cascade="all, delete, delete-orphan")
-    reviews = relationship('Review', backref="user",
-                           cascade="all, delete, delete-orphan")
+
+    if getenv('HBNB_TYPE_STORAGE') is not None:
+
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship("Place", cascade='all, delete, delete-orphan',
+                              backref="user", passive_deletes=True)
+        reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                               backref="user", passive_deletes=True)
+
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
