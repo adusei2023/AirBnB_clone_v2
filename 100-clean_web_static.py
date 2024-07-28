e7c9368b79372474f07afe73198e780db3b30278
gloria506 <gloriaboateng2011@gmail.com>
2024-07-28 22:29:09 +0000

    deploy

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,17 +1,21 @@
+env.hosts = ["100.24.72.218", "52.91.137.12"]
 #!/usr/bin/python3
-# Fabfile to delete out-of-date archives.
+"""
+Deletes out-of-date archives
+fab -f 100-clean_web_static.py do_clean:number=2
+    -i ssh-key -u ubuntu > /dev/null 2>&1
+"""
+
 import os
 from fabric.api import *
 
-env.hosts = ["54.173.213.141", "3.80.19.61"]
+env.hosts = ['100.24.72.218', '52.91.137.12']
 
 
 def do_clean(number=0):
     """Delete out-of-date archives.
-
     Args:
         number (int): The number of archives to keep.
-
     If number is 0 or 1, keeps only the most recent archive. If
     number is 2, keeps the most and second-most recent archives,
     etc.
